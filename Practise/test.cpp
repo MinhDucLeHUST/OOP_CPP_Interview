ef3bb64c0b3e6a7a79424255aec76a729b32dfba
MinhDucLeHUST <minhducle2082k@gmail.com>
2023-12-29 17:59:53 +0700

    can not storage data

--- Practise/test.cpp
+++ Practise/test.cpp
@@ -34,29 +34,31 @@ class Management {
         string status;
     };
     int chooseTask;
-    Task insertTask;
-    json dataJson;
 
    public:
+    json dataJson;
+    Task insertTask;
+    vector<Task> saveData;
     Management(string fileName) {
-        cout << "Choose your action: " << endl;
-        cout << "\t1. Assign new task\n\t2. Remove task\n\t3. Task was resolved\n\t4. List all tasks" << endl;
+        cout << "==== Choose number of your action ====" << endl;
+        cout << "\t1. Assign new task\n\t2. Storage data\n\t3. Task was resolved\n\t4. List all tasks\n\t5. Remove task" << endl;
         cin >> chooseTask;
         // Init init_obj;
         // string fileName = init_obj.setName();
         switch (chooseTask) {
             case 1:
-                assignTask();
+                assignTask(fileName);
                 break;
             case 2:
-                storeData(fileName);
+                storeData(fileName, saveData);
+                break;
             default:
                 cout << "Error !!!\n";
                 break;
         }
     }
-    vector<Task> saveData;
-    void assignTask() {
+
+    void assignTask(string nameOfFile) {
         cout << "Task: ";
         cin >> insertTask.taskName;
 
@@ -68,36 +70,6 @@ class Management {
         cout << "Status: ";
         cin >> insertTask.status;
 
-        // dataJson[insertTask.taskName]["name"] = insertTask.taskName;
-        // dataJson[insertTask.taskName]["expireDate"] = insertTask.expirationDate;
-        // dataJson[insertTask.taskName]["state"] = insertTask.status;
-    }
-    void removeTask(string taskDelete, string fileJson) {
-        ifstream inputFile(fileJson);
-        json jsonData;
-        inputFile >> jsonData;
-        inputFile.close();
-
-        // Remove data based on key
-        string keyToRemove = "age";
-        if (jsonData.find(keyToRemove) != jsonData.end()) {
-            jsonData.erase(keyToRemove);
-            cout << "Key '" << keyToRemove << "' and its associated value have been removed from the JSON." << endl;
-        } else {
-            cout << "Key '" << keyToRemove << "' not found in the JSON." << endl;
-        }
-
-        // Write the updated JSON back to the file
-        ofstream outputFile(fileJson);
-        outputFile << jsonData.dump(4);  // The '4' is for pretty printing
-        outputFile.close();
-    }
-    // bool isDoneTask(){
-
-    // }
-    // void listAllTask();
-
-    void storeData(string nameOfFile) {
         saveData.push_back(insertTask);
         for (auto& i : saveData) {
             cout << "--- Task Name: " << i.taskName << ", Expiration Date: " << i.expirationDate << ", Status: " << i.status << endl;
@@ -116,21 +88,45 @@ class Management {
         ofstream file(nameOfFile, ios::app);
         file << jsonData.dump(4);  // Pretty-print with 4 spaces
         file.close();
+        // dataJson[insertTask.taskName]["name"] = insertTask.taskName;
+        // dataJson[insertTask.taskName]["expireDate"] = insertTask.expirationDate;
+        // dataJson[insertTask.taskName]["state"] = insertTask.status;
+    }
+    void removeTask(string taskDelete, string fileJson) {
+    }
+
+    void storeData(string nameOfFile, vector<Task> dataToJson) {
+        // saveData.push_back(insertTask);
+        // cout << "check inside number 2" << endl;
+        for (auto& i : dataToJson) {
+            cout << "--- Task Name: " << i.taskName << ", Expiration Date: " << i.expirationDate << ", Status: " << i.status << endl;
+        }
+
+        json jsonData;
+
+        for (const auto& task : dataToJson) {
+            json taskData;
+            taskData["taskName"] = task.taskName;
+            taskData["expirationDate"] = task.expirationDate;
+            taskData["status"] = task.status;
+            jsonData.push_back(taskData);
+        }
+
+        ofstream file(nameOfFile, ios::app);
+        file << jsonData.dump(4);  // Pretty-print with 4 spaces
+        file.close();
     }
 };
 
 int main() {
     Init initObj;
     string jsonFile = initObj.setName();
-    char stop = 'f';
-    while (stop != 't') {
+    char stop = 'n';
+    while (stop != 'y') {
         Management managementObj(jsonFile);
-        cout << "Press 't' to stop (if you want)";
+        cout << "Press 'y' to stop, 'n' to continue: ";
         cin >> stop;
     }
-    // managementObj.assignTask();
-    // managementObj.assignTask();
-    // managementObj.storeData(nameJson);
 
     return 0;
 }
