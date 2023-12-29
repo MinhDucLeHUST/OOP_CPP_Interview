0125ddd9e9ae31f12b8fe200dc9fe561076940b0
MinhDucLeHUST <minhducle2082k@gmail.com>
2023-12-29 17:17:38 +0700

    develop fail

--- Practise/main.cpp
+++ Practise/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
+#include <stack>
 
 #include "iostream"
 #include "nlohmann/json.hpp"
@@ -10,61 +11,100 @@ using namespace std;
 
 using json = nlohmann::json;
 
-class Management {
+class Init {
    private:
-    typedef struct {
-        int day;
-        int month;
-        int year;
-    } date_t;
-    string task;
-    string state;
+    // string nameFile;
 
    public:
+    string setName() {
+        string fileJson = "";
+        cout << "Insert name of file: ";
+        cin >> fileJson;
+        fileJson = fileJson + ".json";
+        return fileJson;
+    }
+};
+
+class Management {
+   private:
+    string expireDate;
+    string task;
+    string state;
+    json data;
+
+   public:
+    // Management() {
+    //     cout << "Choose your action: " << endl;
+    //     cout << "\tAssign new task\n\tRemove task\n\tTask was resolved\n\tList all tasks" << endl;
+    //     cout << "What do you want to do?";
+    //     cin << task;
+    // }
     void assignTask() {
-        date_t expireDate;
         cout << "Task: ";
         cin >> task;
 
-        cout << "Expiration date: " << endl;
-        cout << "Day: ";
-        cin >> expireDate.day;
-        cout << "Month: ";
-        cin >> expireDate.month;
-        cout << "Year: ";
-        cin >> expireDate.year;
+        cout << "Expiration date (dd/mm/yyyy): ";
+        cin >> expireDate;
 
         cout << "List of status: " << endl;
         cout << "\tOpen\n\tHolding\n\tReopen\n\tDone" << endl;
         cout << "Status: ";
         cin >> state;
 
-        json data;
         data[task]["name"] = task;
-        data[task]["expireDate"]["day"] = expireDate.day;
-        data[task]["expireDate"]["month"] = expireDate.month;
-        data[task]["expireDate"]["year"] = expireDate.year;
+        data[task]["expireDate"] = expireDate;
         data[task]["state"] = state;
-
-        // Write the JSON object to a file
-        ofstream file("data.json", ios::app);
-        file << data.dump(4);
-        file.close();
-
-        cout << "Data has been stored in data.json" << endl;
     }
-    void removeTask() {
+    void removeTask(string taskDelete, string fileJson) {
+        ifstream inputFile(fileJson);
+        json jsonData;
+        inputFile >> jsonData;
+        inputFile.close();
+
+        // Remove data based on key
+        string keyToRemove = "age";
+        if (jsonData.find(keyToRemove) != jsonData.end()) {
+            jsonData.erase(keyToRemove);
+            cout << "Key '" << keyToRemove << "' and its associated value have been removed from the JSON." << endl;
+        } else {
+            cout << "Key '" << keyToRemove << "' not found in the JSON." << endl;
+        }
+
+        // Write the updated JSON back to the file
+        ofstream outputFile(fileJson);
+        outputFile << jsonData.dump(4);  // The '4' is for pretty printing
+        outputFile.close();
     }
-    // bool isDoneTask();
+    // bool isDoneTask(){
+
+    // }
     // void listAllTask();
 
-    // void storeData()
+    void storeData(string nameOfFile) {
+        stack<json> dataStack;
+        dataStack.push(data);
+        json jsonArray;
+        while (!dataStack.empty()) {
+            jsonArray.push_back(dataStack.top());
+            cout << "--- " << jsonArray << endl;
+            dataStack.pop();
+        }
+
+        ofstream file(nameOfFile, ios::app);
+        file << jsonArray.dump(4);
+        file.close();
+
+        cout << "=== Data has been stored in data.json ===" << endl;
+    }
 };
 
 int main() {
-    Management manageTask;
-    manageTask.assignTask();
+    Management managementObj;
+    Init initObj;
+    string nameJson = initObj.setName();
+    managementObj.assignTask();
+    managementObj.assignTask();
+    managementObj.storeData(nameJson);
 
-    // manageTask.assignTask();
     return 0;
 }
--- Practise/test.cpp
+++ Practise/test.cpp
@@ -1,56 +1,136 @@
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
-#include <iostream>
+#include <stack>
 
+#include "iostream"
 #include "nlohmann/json.hpp"
+#include "string"
+#include "vector"
+
+using namespace std;
 
 using json = nlohmann::json;
 
-int main() {
-    // Read existing JSON data from the file
-    // std::ifstream file("input.json");
-    // json existingData;
-    // file >> existingData;
-    // file.close();
-    std::ifstream inputFile("input.json");
-    if (inputFile.is_open()) {
-        std::ifstream file("input.json");
-        json existingData;
-        file >> existingData;
-        file.close();
-        json newData = {
-            {"expireDate", {{"day", 20}, {"month", 12}, {"year", 2024}}},
-            {"name", "MinhDuc"},
-            {"state", "InProgress"}};
-        existingData["bug10"] = newData;
+class Init {
+   private:
+    // string nameFile;
 
-        // Write the updated JSON object back to the file
-        std::ofstream outFile("input.json", std::ios::app);
-        outFile << std::setw(4) << existingData;
-        outFile.close();
-
-        std::cout << "New data has been added to input.json" << std::endl;
-        inputFile.close();
-    } else {
-        std::ofstream fileCreate("input.json", std::ios_base::app);
-        fileCreate << "[" << std::endl;
-        fileCreate << "]" << std::endl;
-        // Close the file
-        fileCreate.close();
-        std::cout << "Characters { and } have been added to data.json" << std::endl;
+   public:
+    string setName() {
+        string fileJson = "";
+        cout << "Insert name of file: ";
+        cin >> fileJson;
+        fileJson = fileJson + ".json";
+        return fileJson;
     }
-    // Add new data to the JSON object
-    // json newData = {
-    //     {"expireDate", {{"day", 20}, {"month", 12}, {"year", 2024}}},
-    //     {"name", "bug10"},
-    //     {"state", "InProgress"}};
-    // existingData["bug10"] = newData;
+};
 
-    // // Write the updated JSON object back to the file
-    // std::ofstream outFile("data.json");
-    // outFile << std::setw(4) << existingData;
-    // outFile.close();
+class Management {
+   private:
+    struct Task {
+        string taskName;
+        string expirationDate;
+        string status;
+    };
+    int chooseTask;
+    Task insertTask;
+    json dataJson;
 
-    // std::cout << "New data has been added to data.json" << std::endl;
+   public:
+    Management(string fileName) {
+        cout << "Choose your action: " << endl;
+        cout << "\t1. Assign new task\n\t2. Remove task\n\t3. Task was resolved\n\t4. List all tasks" << endl;
+        cin >> chooseTask;
+        // Init init_obj;
+        // string fileName = init_obj.setName();
+        switch (chooseTask) {
+            case 1:
+                assignTask();
+                break;
+            case 2:
+                storeData(fileName);
+            default:
+                cout << "Error !!!\n";
+                break;
+        }
+    }
+    vector<Task> saveData;
+    void assignTask() {
+        cout << "Task: ";
+        cin >> insertTask.taskName;
+
+        cout << "Expiration date (dd/mm/yyyy): ";
+        cin >> insertTask.expirationDate;
+
+        cout << "List of status: " << endl;
+        cout << "\tOpen\n\tHolding\n\tReopen\n\tDone" << endl;
+        cout << "Status: ";
+        cin >> insertTask.status;
+
+        // dataJson[insertTask.taskName]["name"] = insertTask.taskName;
+        // dataJson[insertTask.taskName]["expireDate"] = insertTask.expirationDate;
+        // dataJson[insertTask.taskName]["state"] = insertTask.status;
+    }
+    void removeTask(string taskDelete, string fileJson) {
+        ifstream inputFile(fileJson);
+        json jsonData;
+        inputFile >> jsonData;
+        inputFile.close();
+
+        // Remove data based on key
+        string keyToRemove = "age";
+        if (jsonData.find(keyToRemove) != jsonData.end()) {
+            jsonData.erase(keyToRemove);
+            cout << "Key '" << keyToRemove << "' and its associated value have been removed from the JSON." << endl;
+        } else {
+            cout << "Key '" << keyToRemove << "' not found in the JSON." << endl;
+        }
+
+        // Write the updated JSON back to the file
+        ofstream outputFile(fileJson);
+        outputFile << jsonData.dump(4);  // The '4' is for pretty printing
+        outputFile.close();
+    }
+    // bool isDoneTask(){
+
+    // }
+    // void listAllTask();
+
+    void storeData(string nameOfFile) {
+        saveData.push_back(insertTask);
+        for (auto& i : saveData) {
+            cout << "--- Task Name: " << i.taskName << ", Expiration Date: " << i.expirationDate << ", Status: " << i.status << endl;
+        }
+
+        json jsonData;
+
+        for (const auto& task : saveData) {
+            json taskData;
+            taskData["taskName"] = task.taskName;
+            taskData["expirationDate"] = task.expirationDate;
+            taskData["status"] = task.status;
+            jsonData.push_back(taskData);
+        }
+
+        ofstream file(nameOfFile, ios::app);
+        file << jsonData.dump(4);  // Pretty-print with 4 spaces
+        file.close();
+    }
+};
+
+int main() {
+    Init initObj;
+    string jsonFile = initObj.setName();
+    char stop = 'f';
+    while (stop != 't') {
+        Management managementObj(jsonFile);
+        cout << "Press 't' to stop (if you want)";
+        cin >> stop;
+    }
+    // managementObj.assignTask();
+    // managementObj.assignTask();
+    // managementObj.storeData(nameJson);
 
     return 0;
 }
