3a7efff8e00f6e284b1f07930a88523fc57a1e7c
MinhDucLeHUST <minhducle2082k@gmail.com>
2024-01-05 17:51:26 +0700

    bug at assignTask func

--- Practise/JsonFileLister.cpp
+++ Practise/JsonFileLister.cpp
@@ -1,45 +1,29 @@
-#include <dirent.h>
-
-#include <cstring>
+#include <iomanip>
 #include <iostream>
-#include <string>
-#include <vector>
+#include <sstream>
 
-class JsonFileLister {
-   public:
-    void listJsonFilesInCurrentDirectory() {
-        std::string folderPath = ".";  // Assuming the code file is in the same directory as the JSON files
-
-        DIR* dir;
-        struct dirent* ent;
-        bool foundJsonFile = false;
-        if ((dir = opendir(folderPath.c_str())) != NULL) {
-            while ((ent = readdir(dir)) != NULL) {
-                if (hasJsonExtension(ent->d_name)) {
-                    std::cout << ent->d_name << std::endl;
-                    foundJsonFile = true;
-                }
-            }
-            closedir(dir);
-        } else {
-            std::cerr << "Error opening directory" << std::endl;
-        }
-
-        if (!foundJsonFile) {
-            std::cout << "No JSON files found in the directory." << std::endl;
-        }
+bool compareDates(const std::string& dateStr1, const std::string& dateStr2) {
+    std::tm tm1 = {}, tm2 = {};
+    std::istringstream ss1(dateStr1), ss2(dateStr2);
+    ss1 >> std::get_time(&tm1, "%d/%m/%Y");
+    ss2 >> std::get_time(&tm2, "%d/%m/%Y");
+    if (ss1.fail() || ss2.fail()) {
+        throw std::runtime_error("Failed to parse date");
     }
-
-   private:
-    bool hasJsonExtension(const char* fileName) {
-        const char* dot = strrchr(fileName, '.');
-        return dot && !strcmp(dot, ".json");
-    }
-};
+    time_t t1 = std::mktime(&tm1);
+    time_t t2 = std::mktime(&tm2);
+    return t1 < t2;
+}
 
 int main() {
-    JsonFileLister lister;
-    lister.listJsonFilesInCurrentDirectory();
+    std::string dateStr1 = "20/03/1993";
+    std::string dateStr2 = "19/07/1993";
+
+    if (compareDates(dateStr1, dateStr2)) {
+        std::cout << dateStr1 << " is before " << dateStr2 << std::endl;
+    } else {
+        std::cout << dateStr1 << " is after or the same as " << dateStr2 << std::endl;
+    }
 
     return 0;
 }
--- Practise/test.cpp
+++ Practise/test.cpp
@@ -1,5 +1,5 @@
 /*
-    đang phát triển các tính năng lưu trữ data, quay ngược lại các bước trước đó, xử lý với files có sẵn hoặc chưa
+    đang lỗi ở func: assignTask, vì đang phát triển tính năng phát triển trùng lặp tên của task
 */
 
 #include <dirent.h>
@@ -33,6 +33,8 @@ struct Task {
 class Management {
    private:
     int chooseTask;
+    string befOrPast;
+    int chooseKeyRemove;
     vector<Task> vectorData;
 
     bool hasJsonExtension(const char* fileName) {
@@ -40,9 +42,20 @@ class Management {
         return dot && !strcmp(dot, ".json");
     }
 
-    void storeData(string nameOfFile, const std::vector<Task>& taskList) {
-        cout << "[2] Data was saved" << endl;
+    bool compareDates(const string& dateStr1, const string& dateStr2) {
+        tm tm1 = {}, tm2 = {};
+        istringstream ss1(dateStr1), ss2(dateStr2);
+        ss1 >> get_time(&tm1, "%d/%m/%Y");
+        ss2 >> get_time(&tm2, "%d/%m/%Y");
+        if (ss1.fail() || ss2.fail()) {
+            throw runtime_error("Failed to parse date");
+        }
+        time_t t1 = mktime(&tm1);
+        time_t t2 = mktime(&tm2);
+        return t1 < t2;  // return true if date was insert before date in json
+    }
 
+    void storeData(string nameOfFile, const std::vector<Task>& taskList) {
         json jsonData;
 
         for (const auto& task : taskList) {
@@ -55,6 +68,7 @@ class Management {
 
         ofstream file(nameOfFile);
         file << jsonData.dump(4);  // Pretty-print with 4 spaces
+        // cout << "Data was saved" << endl;
         file.close();
     }
 
@@ -123,26 +137,17 @@ class Management {
         return fileJson;
     }
 
-    bool isJsonFileExist(string nameJsonFileToCheck) {
-        string folderPath = ".";  // Replace with the actual path to your folder
-        for (const auto& entry : fs::directory_iterator(folderPath)) {
-            if (entry.path().filename() == nameJsonFileToCheck && entry.path().extension() == ".json") {
-                return true;
-            }
-        }
-        return false;
-    }
-
    public:
     Management() {
         cout << "\033[1;32mWelcome to my app\033[0m" << endl;
         string fileName = listJson();
         getDataInExistFile(fileName);
         while (1) {
-            cout << "============ MENU ============" << endl;
-            cout << "\t\033[33m1.\033[0m Assign new task\n\t\033[33m2.\033[0m Store data\n\t\033[33m3.\033[0m Edit data\n\t\033[33m4.\033[0m List all tasks\n\t\033[33m5.\033[0m Remove task\n\t\033[33m6.\033[0m Back\n\t\033[33m7.\033[0m Quit" << endl;
+            cout << "\033[1;1m============\033[0m \033[1;32mMENU\033[0m \033[1;1m============\033[0m" << endl;
+            cout << "\t\033[33m1.\033[0m Assign new task\n\t\033[33m2.\033[0m Store data\n\t\033[33m3.\033[0m Edit data\n\t\033[33m4.\033[0m List all tasks\n\t\033[33m5.\033[0m Remove task\n\t\033[33m6.\033[0m Back\n\t\033[33m7.\033[0m Exit" << endl;
             cout << "---> Enter active (\033[33mnumber\033[0m): ";
             cin >> chooseTask;
+            cout << "\033[1;1m==============================\033[0m" << endl;
             // Does the value entered from the keyboard have the format of the variable?
             if (cin.fail()) {
                 cin.clear();                                                    // Clear the error state
@@ -153,6 +158,7 @@ class Management {
                     assignTask(fileName);
                     break;
                 case 2:
+                    cout << "\033[33m[2] \033[0m Saving data" << endl;
                     storeData(fileName, vectorData);
                     break;
                 case 3:
@@ -160,16 +166,19 @@ class Management {
                     break;
                 case 4:
                     listAllTasks(fileName);
-                    // listJson();
                     break;
                 case 5:
                     removeByTask(fileName, false);
                     break;
                 case 6:
+                    // set condition
+                    cout << "\033[33m[6] \033[0mBack" << endl;
                     vectorData.clear();  // clear data inside of vector
                     fileName = listJson();
+                    getDataInExistFile(fileName);
                     break;
                 case 7:
+                    cout << "\033[33m[7] \033[0mExit" << endl;
                     exit(0);
                 default:
                     cout << "\033[1;31mError !!! Please re-enter.\033[0m\n";
@@ -179,9 +188,23 @@ class Management {
     }
     void assignTask(string nameOfFile) {
         Task insertTask;
-        cout << "[1] New task: ";
+        cout << "\033[33m[1] \033[0mAssign new task (press 'q' to back)" << endl;
+        cout << "New task: ";
         cin >> insertTask.taskName;
 
+        if (insertTask.taskName == "q") {
+            return;
+        }
+        ifstream input(nameOfFile);
+        json jsonData;
+        input >> jsonData;
+        input.close();
+        for (auto it = jsonData.begin(); it != jsonData.end(); ++it) {
+            if (it->at("taskName") == insertTask.taskName) {
+                cout << "This task \033[1;31m" << insertTask.taskName << "\033[0m was exist in file, pls re-enter another name" << endl;
+                assignTask(nameOfFile);
+            }
+        }
         cout << "Expiration date (dd/mm/yyyy): ";
         cin >> insertTask.expirationDate;
 
@@ -193,34 +216,130 @@ class Management {
         vectorData.push_back(insertTask);
     }
     void removeByTask(string nameOfFile, bool flag) {
-        string taskRemove;
-        cout << "[5] Enter name of task to remove: ";
-        cin >> taskRemove;
-        ifstream input(nameOfFile);
-        json jsonData;
-        input >> jsonData;
-        input.close();
+        bool outWhile = false;
+        while (1) {
+            cout << "\033[33m[5] \033[0mRemove task inside file (enter '4' to back to Main menu)" << endl;
+            cout << "\t[5]\033[33m[1] \033[0m Remove by name of task." << endl;
+            cout << "\t[5]\033[33m[2] \033[0m Remove by status of task." << endl;
+            cout << "\t[5]\033[33m[3] \033[0m Remove by expiry date of task." << endl;
+            cout << "\t[5]\033[33m[4] \033[0m Back." << endl;
+            cout << "\t[5]\033[33m[5] \033[0m Exit." << endl;
+            cout << "---> Enter choice (\033[33mnumber\033[0m): ";
+            cin >> chooseKeyRemove;
 
-        for (auto it = jsonData.begin(); it != jsonData.end(); ++it) {
-            if (it->at("taskName") == taskRemove) {
-                flag = true;  // enable flag to notice that is exist in json's file
-                jsonData.erase(it);
-                break;
+            string strDel;
+            if (cin.fail()) {
+                cin.clear();                                                    // Clear the error state
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Clear the input buffer
             }
-        }
-        // flag to check 'taskRemove' is exist in json's file
-        if (!flag) {
-            cout << "\033[1;31mError !!! \033[0m Could not find " << taskRemove << " in json's file." << endl;
-            return;
-        }
 
-        ofstream output(nameOfFile);
-        output << setw(4) << jsonData << endl;
-        output.close();
+            ifstream input(nameOfFile);
+            json jsonData;
+            input >> jsonData;
+            input.close();
+            auto it = jsonData.begin();
+            switch (chooseKeyRemove) {
+                case 1:
+                    cout << "Enter task (press \033[33m'q' \033[0m to go back): ";
+                    cin >> strDel;
+                    if (strDel == "q") {
+                        removeByTask(nameOfFile, false);
+                    }
+
+                    for (auto it = jsonData.begin(); it != jsonData.end(); ++it) {
+                        if (it->at("taskName") == strDel) {
+                            flag = true;  // enable flag to notice that is exist in json's file
+                            jsonData.erase(it);
+                            break;
+                        }
+                    }
+
+                    // flag to check 'strDel' is exist in json's file
+                    if (!flag) {
+                        cout << "Could not find task: \033[1;31m" << strDel << "\033[0m in json's file to remove." << endl;
+                        // return;
+                    }
+                    break;
+                case 2:
+                    cout << "Enter status (press \033[33m'q' \033[0m to go back): ";
+                    cin >> strDel;
+                    if (strDel == "q") {
+                        removeByTask(nameOfFile, false);
+                    }
+
+                    while (it != jsonData.end()) {
+                        if (it->at("status") == strDel) {
+                            flag = true;
+                            it = jsonData.erase(it);
+                        } else {
+                            ++it;
+                        }
+                    }
+
+                    // flag to check 'strDel' is exist in json's file
+                    if (!flag) {
+                        cout << "Could not find status: \033[1;31m" << strDel << "\033[0m in json's file to remove." << endl;
+                        // return;
+                    } else {
+                        cout << "All tasks have status \033[32m" << strDel << "\033[0m was removed!" << endl;
+                    }
+                    break;
+                case 3:
+
+                    cout << "Enter date (dd/mm/yyyy) (press 'q' to go back): ";
+                    cin >> strDel;
+                    cout << "Do you want to delete tasks in the \033[33mpast \033[0m or in the \033[33mfuture \033[0m (past/future): ";
+                    cin >> befOrPast;
+                    if (strDel == "q") {
+                        removeByTask(nameOfFile, false);
+                    }
+                    if (befOrPast == "future") {
+                        for (auto it = jsonData.begin(); it != jsonData.end(); ++it) {
+                            if (compareDates(strDel, it->at("expirationDate"))) {
+                                // if (== strDel) {
+                                flag = true;  // enable flag to notice that is exist in json's file
+                                jsonData.erase(it);
+                                break;
+                            }
+                        }
+                    } else if (befOrPast == "past") {
+                        for (auto it = jsonData.begin(); it != jsonData.end(); ++it) {
+                            if (!compareDates(strDel, it->at("expirationDate"))) {
+                                // if (== strDel) {
+                                flag = true;  // enable flag to notice that is exist in json's file
+                                jsonData.erase(it);
+                                break;
+                            }
+                        }
+                    } else {
+                        cout << "\033[1;31mInvalid date, pls re-enter\033[0m" << endl;
+                        removeByTask(nameOfFile, false);
+                    }
+
+                    // flag to check 'strDel' is exist in json's file
+                    if (!flag) {
+                        cout << "Could not find date: \033[1;31m" << strDel << "\033[0m in json's file to remove." << endl;
+                        // return;
+                    }
+                    break;
+                case 4:
+                    return;
+                case 5:
+                    exit(0);
+                default:
+                    cout << "\033[1;31mInvalid value, pls re-enter\033[0m" << endl;
+                    break;
+            }
+
+            ofstream output(nameOfFile);
+            output << setw(4) << jsonData << endl;
+            output.close();
+            flag = false;
+        }
     }
 
     void listAllTasks(string nameOfFile) {
-        cout << "[4] List all task" << endl;
+        cout << "\033[33m[4]\033[0m List all task" << endl;
         ifstream input(nameOfFile);
         json jsonData;
         input >> jsonData;
